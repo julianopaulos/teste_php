6875c608ae72ea70662ea41d23d141450cde5ec3
julianopaulos <julianopaulo.santos@hotmail.com>
2022-01-25 07:48:57 -0300

    finalizada última função

--- functions/sequencia_crescente.php
+++ functions/sequencia_crescente.php
@@ -6,13 +6,14 @@ function SEQUENCIACRESCENTE($array){
     }
     $posicao_retirada = 0;
     $sequencial = false;
-    $new_arrays = [];
     while($posicao_retirada <= end($array) && $sequencial === false){
+
         $novoArray = $array;
         unset($novoArray[$posicao_retirada]);
         
         $posicao_retirada++;
         $menor = min($novoArray)-1;
+        
         $sequencia = 0;
         foreach($novoArray as $num){
             if($num > $menor){
@@ -24,9 +25,8 @@ function SEQUENCIACRESCENTE($array){
             }
         }
         if($sequencia === count($novoArray)){
-            $new_arrays[] = $novoArray;
             $sequencial = true;
         }
     }
-    return [$sequencial, $array, $new_arrays];
+    return $sequencial;
 }
